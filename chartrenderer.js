b0810adb2fa4ed25fae67538762dc75eff86a3c1
marfoldi <marfoldi_@hotmail.com>
2015-08-24 22:26:36 +0200

    Updated chartrenderer.js

--- chartrenderer.js
+++ chartrenderer.js
@@ -54,10 +54,10 @@ zoomChart();
 function zoomChart() {
 	// different zoom methods can be used - zoomToIndexes, zoomToDates,
 	// zoomToCategoryValues
-	chart.zoomToIndexes(chartData.length - 40, chartData.length - 1);
+	chart.zoomToDates(chartData.length - 40, chartData.length - 1);
 }
 
-// generate some random data, quite different range
+// generate data
 function generateChartData() {
 	var chartData = {
 		move: [],
@@ -70,43 +70,23 @@ function generateChartData() {
 		var sleep;
 		$.each(this, function(key,value){
 			if(key === "DATE") {
-				date = new Date(value.substring(0,3), value.substring(4,5), value.substring(6,7));
+				date = new Date( parseInt(value.substring(0,4)), parseInt(value.substring(4,6)), parseInt(value.substring(6,8)));
 			}
 			if(key === "m_distance") {
-				distance = (value/1000).toFixed(1);
+				distance = isNaN(value)?0:(value/1000).toFixed(1);
 			}
-			if(key === "s_duration") {
-				sleep = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, value);
-			}
+			//if(key === "s_duration") {
+				//sleep = isNaN(value)?"0:00":new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, value);
+			//}
 		});
 		chartData.move.push({
-			date : date.getDate(),
+			date : date,
 			distance : distance
 		});
-		chartData.sleep.push({
+		/*chartData.sleep.push({
 			date : date.getDate(),
 			sleep : sleep
-		});
+		});*/
 	});
-	/*var chartData = [];
-	var firstDate = new Date();
-	firstDate.setDate(firstDate.getDate() - 5);
-
-	for (var i = 0; i < 1000; i++) {
-		// we create date objects here. In your data, you can have date strings
-		// and then set format of your dates using chart.dataDateFormat
-		// property,
-		// however when possible, use date objects, as this will speed up chart
-		// rendering.
-		var newDate = new Date(firstDate);
-		newDate.setDate(newDate.getDate() + i);
-
-		var visits = Math.round(Math.random() * (40 + i / 5)) + 20 + i;
-
-		chartData.push({
-			date : newDate,
-			visits : visits
-		});
-	}*/
 	return chartData;
 }
